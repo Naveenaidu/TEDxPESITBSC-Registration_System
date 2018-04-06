a4530865f4020f7eff9894a9a09a73e215449505
harshavamsi <harshavamsi096@gmail.com>
2018-04-06 11:05:22 +0530

    everything looks good

--- ingenius/settings/production.py
+++ ingenius/settings/production.py
@@ -9,4 +9,11 @@ ALLOWED_HOSTS = ['127.0.0.1','13.126.226.206','registrations.tedxpesitbsc.com']
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
 STATIC_ROOT = os.environ.get('STATIC_ROOT', None)
