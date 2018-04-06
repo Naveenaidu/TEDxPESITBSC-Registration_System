6553d8458171680baea5bae5767ea0e38d480358
harshavamsi <harshavamsi096@gmail.com>
2018-04-06 10:59:13 +0530

    everything looks good

--- ingenius/settings/production.py
+++ ingenius/settings/production.py
@@ -4,23 +4,9 @@ from ingenius.settings.base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
-
-ALLOWED_HOSTS += [os.environ['DJANGO_HOST_NAME']]
+ALLOWED_HOSTS = ['127.0.0.1','13.126.226.206','registrations.tedxpesitbsc.com']
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ['DBNAME'],
-        'USER': os.environ['DBUSER'],
-        'PASSWORD': os.environ['DBPASS'],
-        'HOST': 'localhost',
-        'PORT': ''
-    }
-}
-
 
 STATIC_ROOT = os.environ['STATIC_ROOT']
