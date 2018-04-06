2be476eaeb1ec6c60196b3eda6dfddef4062c5c8
harshavamsi <harshavamsi096@gmail.com>
2018-04-06 11:03:49 +0530

    everything looks good

--- ingenius/settings/production.py
+++ ingenius/settings/production.py
@@ -9,4 +9,4 @@ ALLOWED_HOSTS = ['127.0.0.1','13.126.226.206','registrations.tedxpesitbsc.com']
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-STATIC_ROOT = os.environ['STATIC_ROOT']
+STATIC_ROOT = os.environ.get('STATIC_ROOT', None)
